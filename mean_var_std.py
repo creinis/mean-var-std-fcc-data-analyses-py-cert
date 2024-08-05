3cd2a0a52bd1b50c8307c8c1ff4367028a79bf4d
creinis <115181527+creinis@users.noreply.github.com>
2024-08-05 14:22:04 +0200

    fix: valueError message

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,7 +3,7 @@ import numpy as np
 def calculate(list):
     
     if len(list) != 9:
-        raise ValueError("List must contain 9 numbers")
+        raise ValueError("List must contain nine numbers.")
     
     matrix = np.array(list).reshape(3,3)
     
